60997e9f9f0e1a9d7813d86d51a31f323a47f18d
Naline Souza <143421320+NalineSouza@users.noreply.github.com>
2024-09-20 09:19:09 -0400

    criando aulas em python
    criandoaulasempython

---
+++ aula33.py
@@ -0,0 +1,7 @@
+numero_1 = input('Digite um número: ')
+numero_2 = input('Digite outro número: ')
+
+int_numero = int(numero_1)
+int_numero = int(numero_2)
+
+print(f'A soma dos números é: {numero_1 + numero_2}')
---
+++ formatstring.py
@@ -0,0 +1,11 @@
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / altura ** 2
+
+linha_1 = f'{nome} tem {altura:.2f} de altura'
+
+print(linha_1)
+print('pesa', peso, 'quilos e seu imc é',)
+print(imc)
+print()
